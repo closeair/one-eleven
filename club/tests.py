dc39c4d9077b99a680be179f81bd69f659f5382d
brez <jbresnik@fuzzproductions.com>
2016-10-02 22:05:45 -0400

    additonal tests for existing functionality

--- club/tests.py
+++ club/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth.models import User
-from .forms import MembershipApplicationForm, VoteForm
+from .forms import MembershipApplicationForm, VoteForm, InsuranceSurveyForm
 from .models import Motion
 import os
 
@@ -69,6 +69,70 @@ class MembershipApplicationFormTests(TestCase):
         })
 
 
+class InsuranceSurveyViewTests(TestCase):
+    def test_insurance_survey_form_render(self):
+        User.objects.create_user('omar', 'omar@ace.org', 'omaramente')
+        self.client.login(username='omar', password='omaramente')
+        response = self.client.get(reverse('insurance'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Do have a current BFR")
+
+
+class InsuranceSurveyFormTests(TestCase):
+    def test_init_invalid_without_entry(self):
+        form = InsuranceSurveyForm({})
+        self.assertFalse(form.is_valid())
+
+    def test_init_valid_with_entry(self):
+        data = {
+            'name': 'Robert Clements',
+            'email': 'roger@ace.org',
+            'phone': '212-123-1232',
+            'birth_date': '1942-09-01',
+            'address': '234 Omar Road',
+            'city': 'Brooklyn',
+            'state_abbreviation': 'NY',
+            'zipcode': '11209',
+            'united_states_citizen': True,
+            'faa_certificate_number': '1234567F',
+            'total_flight_hours': '200',
+            'bfr_expiration': '2017-09-01',
+            'medical_expiration': '2016t-09-01',
+            'reference_name': 'brez',
+            'reference_relation': 'Friend',
+            'reference_phone': '2122231232',
+        }
+        fixture_file = open('%s/club/fixtures/illegitimi.pdf' % os.getcwd(), 'rb')
+        upload_file = {'drivers_license_report': SimpleUploadedFile(fixture_file.name, fixture_file.read())}
+        form = MembershipApplicationForm(data, upload_file)
+        self.assertFalse(form.is_valid())
+
+    def test_init_invalid_missing_data_returns_errors(self):
+        data = {
+            'phone': '212-123-1232',
+            'birth_date': '1942-09-01',
+            'address': '234 Omar Road',
+            'city': 'Brooklyn',
+            'state_abbreviation': 'NY',
+            'zipcode': '11209',
+            'united_states_citizen': True,
+            'faa_certificate_number': '1234567F',
+            'total_flight_hours': '200',
+            'bfr_expiration': '2017-09-01',
+            'medical_expiration': '2016-09-01',
+            'reference_name': 'brez',
+            'reference_relation': 'Friend',
+            'reference_phone': '2122231232',
+        }
+        form = MembershipApplicationForm(data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'name': ['Please enter your full name'],
+            'email': ['Please enter your email address'],
+            'drivers_license_report': ['This field is required.'],
+        })
+
+
 class VoteViewTests(TestCase):
     def setUp(self):
         user = User.objects.create_user('omar', 'omar@ace.org', 'omaramente')
@@ -83,3 +147,17 @@ class VoteViewTests(TestCase):
         response = self.client.get(reverse('vote', kwargs={'motion': '1'}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Motion test")
+
+    def test_init_valid_with_entry(self):
+        data = {
+            'yay': True
+        }
+        form = VoteForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_init_invalid_without_entry(self):
+        form = VoteForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'yay': ['This field is required.'],
+        })
