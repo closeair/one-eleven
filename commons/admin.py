b82158e83f86e26da707a3707ac3881fb5309c32
brez <jbresnik@fuzzproductions.com>
2016-10-04 23:54:04 -0400

    notes mostly on new aircraft models

--- commons/admin.py
+++ commons/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-
 from commons.models import Pilot, Document
+
 
 class PilotInline(admin.StackedInline):
     model = Pilot
     can_delete = False
     verbose_name_plural = 'pilot'
 
+
 class UserAdmin(UserAdmin):
     inlines = (PilotInline, )
+
 
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('name', 'uploaded_by')
@@ -21,7 +23,6 @@ class DocumentAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         obj.save()
-
 
 
 admin.site.unregister(User)
--- commons/models.py
+++ commons/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Pilot(models.Model):
   user = models.OneToOneField(User)
 
   def __unicode__(self):
 	    return self.user.username
+
 
 class Document(models.Model):
   uploaded_by = models.ForeignKey(User, null=True)
@@ -15,9 +17,55 @@ class Document(models.Model):
   def __unicode__(self):
 	    return self.name
 
+
 class SecureDocument(Document):
   pass
+
 
 class SupportingDocument(Document):
   pass
 
+
+class Aircraft(models.Model):
+  avatar = ImageField(upload_to='aircraft')
+  n_number = models.CharField(max_length=10, null=True)
+  model = models.CharField(max_length=255, null=True)
+  location_identifier = models.CharField(max_length=5, null=True)
+  equipment = models.CharField(max_length=255, null=True)
+  color = models.CharField(max_length=25, null=True)
+  true_air_speed_knots = models.CharField(max_length=25, null=True)
+  hp = models.IntegerField()
+  useful_load = models.IntegerField()
+  price_per_hour_usd = models.FloatField()
+  misc_info = models.TextField()
+
+
+class AirworthnessDirective(models.Model):
+    pass
+
+
+
+class Scheduling(models.Model):
+    # pilot
+    # aircraft
+    # start
+    # end
+    # destination
+    # notes
+    pass
+
+
+class Flight(models.Model):
+    # fuel at start
+    # tach (or hobbs)
+    # pilot
+    # aircraft
+    # scheduling
+    pass
+
+
+
+
+
+
+class
